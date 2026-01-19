c3e6143b6456e367a7a794df77ff5380e94ad67a
cam-andersen <s235660@dtu.dk>
2026-01-19 17:46:27 +0100

    Not done yet

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,3 +1,10 @@
+// === MainWindow.axaml.cs controls how the program behaves === 
+/* 
+This includes:
+logging in, creating admin users, showing previous orders,
+placing irders, saving orders and  sending order to the robot
+*/ 
+
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -13,39 +20,43 @@ using SystemLogin;
 
 namespace Login;
 
+// === This controls the main window that the user sees ===
 public partial class MainWindow : Window, INotifyPropertyChanged
 {
+    // === This handles the log in and users, the link to the database and the data of the logged in user ===
     private AccountService _accountService;
     private AppDbContext _appDbContext;
     private Account? _currentUser;
 
+    // === Automatically updates the lists shown ===. 
     public ObservableCollection<OrderViewModel> PreviousOrders { get; } = new();
     public ObservableCollection<ProductViewModel> AvailableProducts { get; } = new();
     public ObservableCollection<ProductViewModel> OrderLines { get; } = new();
-    
     public ObservableCollection<OrderLine> DatabaseOrderLines { get; } = new();
-    
-    // === Viser seneste ordre i "Database"-fanen ===
     public ObservableCollection<ProductViewModel> LastOrderProducts { get; } = new();
 
+    // === Links the buttons to their actions === 
     public RelayCommand ProcessOrderCommand { get; }
     public RelayCommand ConfirmOrderCommand { get; }
 
-    
+    // === Automatically updates the values shown ===
     public event PropertyChangedEventHandler? PropertyChanged;
     private void OnPropertyChanged([CallerMemberName] string? name = null)
         => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 
+    // === If the user is logged in or out and some actions are available === 
     public string CurrentUserId => _currentUser?.Username ?? "";
     public bool IsLoggedIn => _currentUser != null;
     public bool IsLoggedOut => _currentUser == null;
 
+    // === Links the buttons to their actions ===
     public RelayCommand<ProductViewModel> AddToOrderCommand { get; }
     public RelayCommand<ProductViewModel> RemoveFromOrderCommand { get; }
     public RelayCommand<ProductViewModel> IncreaseQtyCommand { get; }
     public RelayCommand<ProductViewModel> DecreaseQtyCommand { get; }
     public RelayCommand PlaceOrderCommand { get; }
 
+    // === Controls the active tab which is open, so the program knows what to display === 
     private int _selectedTabIndex;
     public int SelectedTabIndex
     {
@@ -61,14 +72,18 @@ public partial class MainWindow : Window, INotifyPropertyChanged
         }
     }
 
+    // === This will run when opening the program === 
     public MainWindow()
     {
+        // === Reads the design from the MainWindow.axaml === 
         InitializeComponent();
         DataContext = this;
-
+        
+        // === Makes the system ready to working ===
         InitializeServices();
         Loaded += OnLoaded;
 
+        // === When adding/removing a product, the quantity changes. But never below 0 ===
         IncreaseQtyCommand = new RelayCommand<ProductViewModel>(p => p.Quantity++);
         DecreaseQtyCommand = new RelayCommand<ProductViewModel>(p =>
         {
@@ -76,118 +91,152 @@ public partial class MainWindow : Window, INotifyPropertyChanged
                 p.Quantity--;
         });
 
+        // === Adding and deleting from the order === 
         AddToOrderCommand = new RelayCommand<ProductViewModel>(AddToOrder);
         RemoveFromOrderCommand = new RelayCommand<ProductViewModel>(RemoveFromOrder);
+
+        // === The place order only works if the user is logged in and there is items in the order === 
         PlaceOrderCommand = new RelayCommand(
             PlaceOrder,
             () => IsLoggedIn && OrderLines.Any(p => p.Quantity > 0)
         );
 
+        // === Sends the order to the robot and deletes the order from the database ===
         ProcessOrderCommand = new RelayCommand(OnConfirmOrderCompleted);
         ConfirmOrderCommand = new RelayCommand(OnConfirmOrderRemoveFromDatabase);
 
-
+        // === Available products the admin can order ===
         AvailableProducts.Add(new ProductViewModel("Component A"));
         AvailableProducts.Add(new ProductViewModel("Component B"));
         AvailableProducts.Add(new ProductViewModel("Component C"));
     }
 
+    // === When the window opens, it check if the database exists === 
     private async void OnLoaded(object? sender, RoutedEventArgs e)
     {
+        // === if the database does not exists, it is created ===
         if (await EnsureDatabaseCreatedWithExampleDataAsync())
             Log("Database did not exist. Created a new one.");
     }
 
+    // === Initial actions to prepare the systems that the program needs to work ===
     private void InitializeServices()
     {
+        // === Close old database connection === 
         _appDbContext?.Dispose();
+        // === Create new database connection === 
         _appDbContext = new AppDbContext();
+        // === Create the log in system === 
         _accountService = new AccountService(_appDbContext, new PasswordHasher());
     }
 
+    // === The database acts whether it exists or not ===  
     private async Task<bool> EnsureDatabaseCreatedWithExampleDataAsync()
     {
+        // === Check if the database exists. If not, create it and if yes, do nothing ===
         var created = await _appDbContext.Database.EnsureCreatedAsync();
         if (!created) return false;
 
+        // === Establishes a new database so the admin or other users can log in ===
         InitializeServices();
         await _accountService.NewAccountAsync("admin", "admin", true);
         await _accountService.NewAccountAsync("user", "user");
         return true;
     }
+
+    // === Load the latest orders, when the database tab is selected === 
     private void OnDatabaseTabSelected()
     {
         LoadLatestOrderFromDatabase();
     }
-
+    
+    // === Log in === 
     private async void LoginButton_OnClick(object? sender, RoutedEventArgs e)
-    {
+    { 
         LoadOrdersForUser(_currentUser);
         LoadLatestOrderFromDatabase();
         UpdateCanExecute();
 
+        // === Reads the username and password === 
         var username = LoginUsername.Text;
         var password = LoginPassword.Text;
 
+        // === Check if the username exist in the database. If not, show message ===
         if (!await _accountService.UsernameExistsAsync(username))
         {
             Log("Username does not exist.");
             return;
         }
 
+        // === Check if the password is correct for the user. If not, show message === 
         if (!await _accountService.CredentialsCorrectAsync(username, password))
         {
             Log("Password wrong.");
             return;
         }
-
+        
+        // === Get the user from the database and store them as the current user === 
         _currentUser = await _accountService.GetAccountAsync(username);
+
+        // === Show log out button as the user is logged in === 
         LogoutButton.IsVisible = true;
 
+        // === Shows the username that is logged in === 
         Log($"{_currentUser.Username} logged in.");
 
+        // === Removes the text in the log in ===
         LoginUsername.Text = "";
         LoginPassword.Text = "";
 
+        // === Update the display when the log in is complete (buttons, text) ===
         OnPropertyChanged(nameof(IsLoggedIn));
         OnPropertyChanged(nameof(IsLoggedOut));
         OnPropertyChanged(nameof(CurrentUserId));
         UpdateCanExecute();
 
-
+        // === Load and display the previous orders made === 
         LoadOrdersForUser(_currentUser);
         UpdateCanExecute();
     }
 
+    // === Log out ===
     private void LogoutButton_OnClick(object? sender, RoutedEventArgs e)
     {
+        // === Program forgets the user, does not show information for the user and remvoes the log out button === 
         _currentUser = null;
         PreviousOrders.Clear();
         LogoutButton.IsVisible = false;
 
+        // === Update the display when the log out is complete (buttons, text) ===
         OnPropertyChanged(nameof(IsLoggedIn));
         OnPropertyChanged(nameof(IsLoggedOut));
         OnPropertyChanged(nameof(CurrentUserId));
 
+        // === Show message === 
         Log("Logged out.");
         UpdateCanExecute();
     }
 
+    // === Go to log in tab === 
     private void OnGoToLoginClick(object? sender, RoutedEventArgs e)
     {
         SelectedTabIndex = 0;
     }
 
+    // === Add products to the order === 
     private void AddToOrder(ProductViewModel product)
     {
+        // === Cannot go below 0 ===
         if (product.Quantity <= 0) return;
 
+        // === Add the products to the order === 
         OrderLines.Add(new ProductViewModel(product.Name, product.Quantity));
         product.Quantity = 0;
-
+    
         UpdateCanExecute();
     }
 
+    // === Remove items from the order === 
     private void RemoveFromOrder(ProductViewModel product)
     {
         OrderLines.Remove(product);
@@ -470,4 +519,5 @@ public class RelayCommand<T> : ICommand
     {
         CanExecuteChanged?.Invoke(this, EventArgs.Empty); 
     }
+
     }
