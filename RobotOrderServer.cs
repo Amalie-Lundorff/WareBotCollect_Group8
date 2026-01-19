0ab92878ac8f5f0e965fa1e8ce0c84b920238837
cam-andersen <s235660@dtu.dk>
2026-01-19 23:23:52 +0100

    Incl. comments

--- RobotOrderServer.cs
+++ RobotOrderServer.cs
@@ -1,20 +1,26 @@
+// RobotOrderServer.cs is sending the orders to the robot.
 using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
 
-namespace Login;   // ⚠️ vigtigt: samme namespace som MainWindow
+namespace Login;
 
 public static class RobotOrderServer
 {
+    // Receiving a list of components.
     public static void LoadOrder(List<string> components)
     {
+        // Connect to the robot.
         using var client = new TcpClient("172.20.254.203", 30002);
         using var stream = client.GetStream();
 
+        // Sending each action to the robot one at a time.
         foreach (var component in components)
         {
+            // Converts the data.
             var msg = Encoding.ASCII.GetBytes(component + "\n");
             stream.Write(msg, 0, msg.Length);
         }
     }
+
 }
